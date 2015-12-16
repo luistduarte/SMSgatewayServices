e4419fbdf0da57adc7915024c5f57a076cbb5b82
Luís Duarte <lduarte.suil@gmail.com>
2015-12-16 11:23:46 +0000

    Update smsgw.sql

--- smsgw.sql
+++ smsgw.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Máquina: localhost
--- Data de Criação: 05-Out-2015 às 17:33
--- Versão do servidor: 5.5.44-0ubuntu0.14.04.1
--- versão do PHP: 5.5.9-1ubuntu4.13
+-- Data de Criação: 16-Dez-2015 às 12:22
+-- Versão do servidor: 5.5.46-0ubuntu0.14.04.2
+-- versão do PHP: 5.5.9-1ubuntu4.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,42 +23,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `receiver`
---
-
-CREATE TABLE IF NOT EXISTS `receiver` (
-  `receiverAddress` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`receiverAddress`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `Receivers`
---
-
-CREATE TABLE IF NOT EXISTS `Receivers` (
-  `requestid` int(11) NOT NULL,
-  `receiverAddress` int(11) NOT NULL,
-  PRIMARY KEY (`requestid`,`receiverAddress`),
-  KEY `receiverAddress` (`receiverAddress`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `request`
 --
 
 CREATE TABLE IF NOT EXISTS `request` (
   `requestid` int(11) NOT NULL AUTO_INCREMENT,
   `stateid` int(11) NOT NULL,
-  `senderAddress` int(11) NOT NULL,
+  `senderAddress` varchar(20) NOT NULL,
   `body` varchar(250) NOT NULL,
   PRIMARY KEY (`requestid`),
   KEY `stateid` (`stateid`),
-  KEY `senderAddress` (`senderAddress`)
+  KEY `FK_sender_address` (`senderAddress`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `rule`
+--
+
+CREATE TABLE IF NOT EXISTS `rule` (
+  `ruleid` int(11) NOT NULL AUTO_INCREMENT,
+  `serviceid` int(11) NOT NULL,
+  `regex` varchar(200) NOT NULL,
+  PRIMARY KEY (`ruleid`),
+  KEY `serviceid` (`serviceid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -68,18 +57,23 @@ CREATE TABLE IF NOT EXISTS `request` (
 --
 
 CREATE TABLE IF NOT EXISTS `sender` (
-  `senderAddress` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
+  `senderAddress` varchar(20) NOT NULL,
   `notifications` tinyint(1) NOT NULL,
   PRIMARY KEY (`senderAddress`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Extraindo dados da tabela `sender`
+-- Estrutura da tabela `service`
 --
 
-INSERT INTO `sender` (`senderAddress`, `name`, `notifications`) VALUES
-(916056618, 'luis duarte', 1);
+CREATE TABLE IF NOT EXISTS `service` (
+  `serviceid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `serviceurl` varchar(200) NOT NULL,
+  PRIMARY KEY (`serviceid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -91,25 +85,34 @@ CREATE TABLE IF NOT EXISTS `state` (
   `stateid` int(11) NOT NULL AUTO_INCREMENT,
   `description` varchar(100) NOT NULL,
   PRIMARY KEY (`stateid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Extraindo dados da tabela `state`
+--
+
+INSERT INTO `state` (`stateid`, `description`) VALUES
+(1, 'MessageWaiting'),
+(2, 'MessageDelivered'),
+(3, 'MessageAccepted'),
+(4, 'MessageNotAccepted');
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Limitadores para a tabela `Receivers`
---
-ALTER TABLE `Receivers`
-  ADD CONSTRAINT `Receivers_ibfk_1` FOREIGN KEY (`requestid`) REFERENCES `request` (`requestid`),
-  ADD CONSTRAINT `Receivers_ibfk_2` FOREIGN KEY (`receiverAddress`) REFERENCES `receiver` (`receiverAddress`);
-
---
 -- Limitadores para a tabela `request`
 --
 ALTER TABLE `request`
-  ADD CONSTRAINT `request_ibfk_1` FOREIGN KEY (`stateid`) REFERENCES `state` (`stateid`),
-  ADD CONSTRAINT `request_ibfk_2` FOREIGN KEY (`senderAddress`) REFERENCES `sender` (`senderAddress`);
+  ADD CONSTRAINT `FK_sender_address` FOREIGN KEY (`senderAddress`) REFERENCES `sender` (`senderAddress`),
+  ADD CONSTRAINT `request_ibfk_1` FOREIGN KEY (`stateid`) REFERENCES `state` (`stateid`);
+
+--
+-- Limitadores para a tabela `rule`
+--
+ALTER TABLE `rule`
+  ADD CONSTRAINT `rule_ibfk_1` FOREIGN KEY (`serviceid`) REFERENCES `service` (`serviceid`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
